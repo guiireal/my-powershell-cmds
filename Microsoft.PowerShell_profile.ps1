81fdf9b5d14969717f79a41e780e15d0814186c4
guiireal <guii_real@hotmail.com>
2024-06-06 12:45:56 -0300

    wip

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,6 +19,7 @@ Set-Alias -Name pa -Value C:\commands\php-artisan.bat
 Set-Alias -Name pads -Value C:\commands\php-artisan-db-seed.bat
 Set-Alias -Name pam -Value C:\commands\php-artisan-migrate.bat
 Set-Alias -Name pamfs -Value C:\commands\php-artisan-migrate-fresh-seed.bat
+Set-Alias -Name paqw -Value C:\commands\php-artisan-queue-work.bat
 Set-Alias -Name pas -Value C:\commands\php-artisan-serve.bat
 Set-Alias -Name pas8001 -Value C:\commands\php-artisan-serve-port-8001.bat
 Set-Alias -Name pas8002 -Value C:\commands\php-artisan-serve-port-8002.bat
